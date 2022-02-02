ed69312019800b47e484d801cf9821b96d7f3952
kumakuma34 <qgqg264@gmail.com>
2022-02-02 22:38:06 +0900

    feat: made login btn
    Signed-off-by: kumakuma34 <qgqg264@gmail.com>

--- yn/src/App.js
+++ yn/src/App.js
@@ -6,9 +6,14 @@ import './App.css';
 function App() {
   return (
     <div className = "container">
-      <div className = "app">
+      <div className = "head">
         <img className ="logo" alt="logo" src = {imgfile}/>
         <hr width = "100%"></hr>
+        <hr width = "100%"></hr>
+      </div>
+
+      <div className = "topics">
+        <button className ="login">SignIn</button>
       </div>
     </div>
   );
